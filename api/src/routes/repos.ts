315753dae031f0e5258ba1433ecffb8b5f933cbc
Pouya Elishaee <p.elishaee@gmail.com>
2022-10-12 16:59:08 -0400

    return repo data with axios

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,7 @@
 import { Router, Request, Response } from 'express';
+import axios from 'axios';
+import jsonData from '../../data/repos.json';
+const url = 'https://api.github.com/users/silverorange/repos';
 
 export const repos = Router();
 
@@ -8,5 +11,20 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  // res.json([]);
+
+  const repoData = async () => {
+    const response = await axios.get(url);
+    const result = response.data
+      .concat(jsonData)
+      .filter((repo: any) => repo.fork === false);
+
+    return result;
+  };
+
+  repoData()
+    .then((data) => {
+      res.status(200).json({ message: 'Request complete', data });
+    })
+    .catch((err) => err.message);
 });
