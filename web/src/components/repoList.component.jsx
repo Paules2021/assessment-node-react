536fff1cd3d35102656e1a641450148de3fc86e7
Pouya Elishaee <p.elishaee@gmail.com>
2022-10-13 12:44:38 -0400

    add language filter option

--- web/src/components/repoList.component.jsx
+++ web/src/components/repoList.component.jsx
@@ -1,29 +1,74 @@
 import './style.css';
+import { useEffect, useState } from 'react';
+
+const defaultLanguage = 'Select Language';
 
 const RepoList = ({ repos }) => {
+  const [filteredData, setFilteredData] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState(defaultLanguage);
+
   //display repos in reverse chronological order by creation date
   const orderedRepos = repos.sort((x, y) => {
     return new Date(y.created_at).getTime() - new Date(x.created_at).getTime();
   });
 
+  //create arry for unique languge
+  const languages = [];
+  for (const item of repos) {
+    languages.push(item.language);
+  }
+  const uniqueLanguageList = [...new Set(languages)];
+  // console.log('uniqeLanguage', uniqueLanguageList);
+
+  useEffect(() => {
+    // Filter list by language type
+    const list = () => {
+      if (selectedLanguage !== defaultLanguage) {
+        const newList = orderedRepos.filter(
+          (e) => e.language === `${selectedLanguage}`
+        );
+        setFilteredData(newList);
+      } else {
+        setFilteredData(orderedRepos);
+      }
+    };
+    list();
+  }, [orderedRepos, selectedLanguage]);
+
   return (
     <div className="repos_list">
+      <div className="repos_select">
+        <span>Select Language Type: </span>
+        <select onChange={(e) => setSelectedLanguage(e.target.value)}>
+          <option value={defaultLanguage}>{defaultLanguage}</option>
+          {uniqueLanguageList.map((item, index) => {
+            return (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+      </div>
       <table>
         <tbody>
           <tr>
             <th>Repository</th>
             <th>Description</th>
             <th>Language</th>
-            <th>Corks count</th>
+            <th>Forks Count</th>
+            <th>Creation Date</th>
           </tr>
-          {orderedRepos.map((repo) => {
-            const { id, name, description, language, forks_count } = repo;
+          {filteredData.map((repo) => {
+            const { id, name, description, language, forks_count, created_at } =
+              repo;
             return (
               <tr className="repos_item" key={id}>
                 <td>{name}</td>
                 <td>{description}</td>
                 <td>{language}</td>
                 <td>{forks_count}</td>
+                <td>{created_at}</td>
               </tr>
             );
           })}
